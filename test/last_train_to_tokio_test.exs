9444e789a2edf7555786c47d579806ceb04cb4c3
galenzo17 <bereciartua.agustin@gmail.com>
2025-09-15 21:38:41 -0300

    Setup initial Elixir/OTP project structure
    Configure proper OTP application with:
    - Application module with supervision tree
    - Updated mix.exs with application mod configuration
    - Fixed test suite to match expected behavior
    
    Resolves #1

--- test/last_train_to_tokio_test.exs
+++ test/last_train_to_tokio_test.exs
@@ -2,7 +2,7 @@ defmodule LastTrainToTokioTest do
   use ExUnit.Case
   doctest LastTrainToTokio
 
-  test "greets the world" do
-    assert LastTrainToTokio.hello() == :world
+  test "train conductor greeting" do
+    assert LastTrainToTokio.hello() == "Tsugi wa Shibuya desu. Doors will open on the left."
   end
 end
